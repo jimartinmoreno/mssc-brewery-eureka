01ff6d1be6db53981426e058396c3d6f6987c255
Nacho Martin <joseignacio.martinmoreno@gmail.com>
2021-12-07 08:57:13 +0100

    Failover services y otras correcciones

--- src/main/java/guru/sfg/brewery/eureka/msscbreweryeureka/MsscBreweryEurekaApplication.java
+++ src/main/java/guru/sfg/brewery/eureka/msscbreweryeureka/MsscBreweryEurekaApplication.java
@@ -7,9 +7,7 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 @EnableEurekaServer
 @SpringBootApplication
 public class MsscBreweryEurekaApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(MsscBreweryEurekaApplication.class, args);
 	}
-
 }
--- src/test/java/guru/sfg/brewery/eureka/msscbreweryeureka/MsscBreweryEurekaApplicationTests.java
+++ src/test/java/guru/sfg/brewery/eureka/msscbreweryeureka/MsscBreweryEurekaApplicationTests.java
@@ -1,13 +1,46 @@
 package guru.sfg.brewery.eureka.msscbreweryeureka;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 
-@SpringBootTest
+import static org.assertj.core.api.BDDAssertions.then;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class MsscBreweryEurekaApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @LocalServerPort
+    private int port;
+
+    @Autowired
+    private TestRestTemplate testRestTemplate;
+
+    @Value("${spring.security.user.name:netflix}")
+    private String user;
+
+    @Value("${spring.security.user.password:eureka}")
+    private String password;
+
+    @Test
+    public void shouldStartEurekaServer() {
+        //        MultiValueMap<String, String> request = new LinkedMultiValueMap<String, String>();
+        //        request.set("username", "netflix");
+        //        request.set("password", "eureka");
+        //        request.set("grant_type", "password");
+        System.out.println("port = " + port);
+        System.out.println("user = " + user);
+        System.out.println("password = " + password);
+
+        ResponseEntity<String> entity = this.testRestTemplate.withBasicAuth(user, password)
+                .getForEntity("http://localhost:" + this.port + "/eureka/apps", String.class);
+                // .postForObject(SyntheticsConstants.OAUTH_ENDPOINT, request, Map.class);
+
+        then(entity.getStatusCode()).isEqualTo(HttpStatus.OK);
+    }
 
 }
