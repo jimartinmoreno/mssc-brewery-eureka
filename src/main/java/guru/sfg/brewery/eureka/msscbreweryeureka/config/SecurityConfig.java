2a36b3165e6970930747fda71eb431c7cdc732b3
Nacho Martin <joseignacio.martinmoreno@gmail.com>
2022-01-14 12:34:45 +0100

    Zipkin con Logback en formato JSON Limpieza de dependencias duplicadas

--- src/main/java/guru/sfg/brewery/eureka/msscbreweryeureka/config/SecurityConfig.java
+++ src/main/java/guru/sfg/brewery/eureka/msscbreweryeureka/config/SecurityConfig.java
@@ -4,6 +4,10 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
+/**
+ * Configuración basic auth de este service registry Eureka
+ */
+
 @Configuration
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
